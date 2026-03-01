64f0fb92484b32ff7e6e6c476b1b0424ba33611a
developer-abu <officialworkofhurairashaikh01@gmail.com>
2026-03-01 16:28:56 +0530

    done

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
   let success = document.getElementById("success");
   let failure = document.getElementById("failure");
+
   let colorDiv = document.querySelectorAll(".childDiv");
+
+
+
+
+
+
+
   for(let i of colorDiv){
 
    i.addEventListener("click", ()=>{
     navigator.clipboard.writeText(i.textContent)
     .then(()=>{
+success.volume = 0.3;
+success.play();
 
-success.play()
+let actualText= i.textContent;
+
+i.innerHTML=`Copied Successful`;
+
+setTimeout(()=>{
+i.innerHTML = actualText
+},1000)
+
     })
     .catch((err)=>{
-failure.play()
+
+success.volume = 0.3;
+failure.play();
+
+let actualText= i.textContent;
+
+i.textContent =`Copied is not Successful`;
+
+setTimeout(()=>{
+i.textContent = actualText
+},1000)
+
+
     })
    })
 
